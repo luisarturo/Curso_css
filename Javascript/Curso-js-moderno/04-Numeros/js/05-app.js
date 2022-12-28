964f9dbe1c896eadf990e2e03b44b812a5d57e57
Luis A.. Cardenas <lcardenas@elimparcial.com>
2022-12-27 17:40:31 -0700

    Se termino la seccion 4: Numeros en JavaScript

---
+++ Javascript/Curso-js-moderno/04-Numeros/js/05-app.js
@@ -0,0 +1,20 @@
+
+
+
+let puntaje = 5;
+
+puntaje++ // 5
+
+++puntaje // 6
+
+let puntaje2 = 5;
+
+puntaje2-- // 5
+--puntaje2 // 4
+
+let puntaje3 = 5;
+
+puntaje3 =+ 3; // 
+
+puntaje3 =- 3;
+console.log(puntaje);
