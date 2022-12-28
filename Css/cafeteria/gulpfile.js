62774946d1b82ed8daf071ba2706a20aa4be6aff
Luis A.. Cardenas <lcardenas@elimparcial.com>
2022-12-27 18:41:33 -0700

    Se agregaron nuevas dependencias y se agregaron nuevos paquetes

--- Css/cafeteria/gulpfile.js
+++ Css/cafeteria/gulpfile.js
@@ -1,12 +1,15 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css( done ) {
     // Compilar sass
     // pasos: 1 - identificar archivo, 2 - Compilar, 3 - Guardar el .css
 
     src('src/scss/app.scss')
-        .pipe( sass({outputStyle: 'compressed'}) )
+        .pipe( sass() )
+        .pipe( postcss( [ autoprefixer() ]))
         .pipe( dest('build/css'))
 
     done();
